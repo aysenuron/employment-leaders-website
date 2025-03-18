c48be2e061bc8f4b515973dd2812ecc107099362
aysenuron <onaranays@gmail.com>
2025-03-18 15:04:26 +0300

    Add .no-sroll to body when nav in open on mobile.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+const body = document.querySelector("body");
 const mobileNav = document.querySelector(".nav-links");
 const burgerMenuBtn = document.querySelector(".menu-toggle");
 
 burgerMenuBtn.addEventListener("click", () => {
     mobileNav.classList.toggle("active");
+    body.classList.toggle("no-scroll");
 });
