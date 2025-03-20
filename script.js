15f9d8c5262cd0a0b23e13cfdb7e082d626071ef
aysenuron <onaranays@gmail.com>
2025-03-20 11:21:44 +0300

    Adjust mobile menu links and scrolling.

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 const body = document.querySelector("body");
 const mobileNav = document.querySelector(".nav-links");
 const burgerMenuBtn = document.querySelector(".menu-toggle");
+const sections = document.querySelectorAll("section");
+const headerLinks = document.querySelectorAll(".header-links a");
 
 burgerMenuBtn.addEventListener("click", () => {
     mobileNav.classList.toggle("active");
     body.classList.toggle("no-scroll");
+});
+
+headerLinks.forEach(link => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        body.classList.remove("no-scroll");
+        mobileNav.classList.remove("active");
+
+        const targetId = link.getAttribute('href').substring(1);
+
+        sections.forEach(section => {
+            if(section.id === targetId) {
+                section.scrollIntoView({behavior: "smooth"});
+            }
+        });
+    });
 });
